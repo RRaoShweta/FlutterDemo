8e0f91e5932c69811ab46c73756a5946640abcb0
shwrao <shwrao@eclassifiedsgroup.com>
2021-12-20 10:05:36 +0530

    State Widget

---
+++ lib/widget/stateful_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DemoStateFullClass extends StatefulWidget {
+  const DemoStateFullClass({Key? key}) : super(key: key);
+
+  @override
+  _DemoStateFullClassState createState() => _DemoStateFullClassState();
+}
+
+class _DemoStateFullClassState extends State<DemoStateFullClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widget/stateless_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DemoStateLessClass extends StatelessWidget {
+  const DemoStateLessClass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
